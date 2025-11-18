61aa13c1ab7ddbd11bc70b622cdcf022f52d8d21
ISH k pro <princeishimwe754@gmail.com>
2025-11-18 20:04:28 +0200

    Add codes for dtos

--- src/auth/dto/login.dto..ts
+++ src/auth/dto/login.dto..ts
@@ -1,1 +1,12 @@
-export class LoginUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class LoginUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,1 +1,16 @@
-export class RegisterUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
