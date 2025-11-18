a59a67aa4fa02f446a0c62265c267ceb898f412b
ISH k pro <princeishimwe754@gmail.com>
2025-11-18 21:01:52 +0200

    Add codes for authService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/register.dto';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import type { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterUserDto } from './dto/register.dto';
+import { LoginUserDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(registerDto: RegisterUserDto) {
+    const existingUser: User | null = await this.prisma.user.findUnique({
+      where: { email: registerDto.email },
+    });
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hash = await bcrypt.hash(registerDto.password, 10);
+
+    const newUser: User = await this.prisma.user.create({
+      data: {
+        email: registerDto.email,
+        name: registerDto.name,
+        password: hash,
+      },
+    });
+
+    return this.generateToken(newUser);
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(loginDto: LoginUserDto) {
+    const user: User | null = await this.prisma.user.findUnique({
+      where: { email: loginDto.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.generateToken(user);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  generateToken(user: User) {
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return {
+      accessToken: token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    };
   }
 }
