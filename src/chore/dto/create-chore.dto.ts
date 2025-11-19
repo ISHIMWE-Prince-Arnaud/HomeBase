63909939665c1be8c11d4540b6c8f5301d59ff2d
ISH k pro <princeishimwe754@gmail.com>
2025-11-19 21:27:50 +0200

    Add codes for CreateChoreDto

--- src/chore/dto/create-chore.dto.ts
+++ src/chore/dto/create-chore.dto.ts
@@ -1,1 +1,21 @@
-export class CreateChoreDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsInt,
+  IsDateString,
+} from 'class-validator';
+
+export class CreateChoreDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsDateString()
+  @IsOptional()
+  dueDate?: string;
+
+  @IsInt()
+  @IsOptional()
+  assignedToId?: number;
+}
