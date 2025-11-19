3e5277854269d1f6e3ad197921deb244483e0625
ISH k pro <princeishimwe754@gmail.com>
2025-11-19 21:37:16 +0200

    Add PrismaService as a provider

--- src/chore/chore.module.ts
+++ src/chore/chore.module.ts
@@ -1,9 +1,10 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Module } from '@nestjs/common';
 import { ChoreService } from './chore.service';
 import { ChoreController } from './chore.controller';
 
 @Module({
   controllers: [ChoreController],
-  providers: [ChoreService],
+  providers: [ChoreService, PrismaService],
 })
 export class ChoreModule {}
