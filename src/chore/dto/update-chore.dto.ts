c69cbaf85366450347fc6c9d629a1b8b4e6dbd70
ISH k pro <princeishimwe754@gmail.com>
2025-11-19 21:28:23 +0200

    Add codes for UpdateChoreDto

--- src/chore/dto/update-chore.dto.ts
+++ src/chore/dto/update-chore.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChoreDto } from './create-chore.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateChoreDto extends PartialType(CreateChoreDto) {}
+export class UpdateChoreDto {
+  @IsBoolean()
+  isComplete: boolean;
+}
