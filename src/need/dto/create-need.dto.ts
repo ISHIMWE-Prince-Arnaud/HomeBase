61669270c6fdaab084bfd4c9d10c79170ea06262
ISH k pro <princeishimwe754@gmail.com>
2025-11-20 15:01:49 +0200

    Add codes for CreateNeedDto

--- src/need/dto/create-need.dto.ts
+++ src/need/dto/create-need.dto.ts
@@ -1,1 +1,18 @@
-export class CreateNeedDto {}
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateNeedDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
+  quantity?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  category?: string;
+}
