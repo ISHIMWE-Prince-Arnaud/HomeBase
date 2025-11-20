5e4c7d34399df58563045635cb510216672d0534
ISH k pro <princeishimwe754@gmail.com>
2025-11-20 12:46:55 +0200

    Add validation rules to CreateExpenseDto

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,1 +1,39 @@
-export class CreateExpenseDto {}
+import {
+  ArrayMinSize,
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsISO8601,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.01)
+  totalAmount: number;
+
+  // Optional; if omitted, defaults to now() in DB
+  @IsOptional()
+  @IsISO8601()
+  date?: string;
+
+  @IsInt()
+  @Min(1)
+  paidById: number;
+
+  @ArrayNotEmpty()
+  @ArrayMinSize(1)
+  @ArrayUnique()
+  @IsInt({ each: true })
+  @Min(1, { each: true })
+  participants: number[];
+}
