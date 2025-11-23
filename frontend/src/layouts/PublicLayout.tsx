a5c75cafdf65b7de49c1fa21d76377b65d3b8067
ISH k pro <princeishimwe754@gmail.com>
2025-11-23 15:52:42 +0200

    Streamline PublicLayout

--- frontend/src/layouts/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout.tsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function PublicLayout() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-muted/40 p-4">
-      <div className="w-full max-w-md space-y-8">
-        <Outlet />
-      </div>
+    <div>
+      <Outlet />
     </div>
   );
 }
